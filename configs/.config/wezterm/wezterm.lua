676cbeb1b21772e3db7cb045aa443dcf63ff3af5
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-06-04 19:11:27 +0200

    modifs in wezterm

--- configs/.config/wezterm/wezterm.lua
+++ configs/.config/wezterm/wezterm.lua
@@ -17,7 +17,9 @@ local config = {
 	-----------------------------------
 
 	--color_scheme = "Catppuccin", -- Machiatto
-	color_scheme = "Tokyo Night Storm",	
+	--color_scheme = "Tokyo Night Storm",	
+	color_scheme = 'Papercolor Light (Gogh)', -- "Aesthetic Night
+
 	color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors/" },
 	-- Aesthetic Night Colorscheme
 	bold_brightens_ansi_colors = true,
