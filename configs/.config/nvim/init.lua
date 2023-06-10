06a2c80d6928a3896e0567be7c6183b840d5f473
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-06-10 15:33:07 +0200

    added and changed btop and nvim init

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -2,5 +2,6 @@ require('base')
 require('keymaps')
 require('plugins')
 require('lsp-config.language-server')
+--require('debug-config.debug-config')
 
 
--- configs/.config/nvim/plugin/packer_compiled.lua
+++ configs/.config/nvim/plugin/packer_compiled.lua
@@ -249,6 +249,12 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/eduardoneville82/.local/share/nvim/site/pack/packer/start/vim-jukit",
     url = "https://github.com/luk400/vim-jukit"
+  },
+  ["which-key.nvim"] = {
+    config = { "\27LJ\2\nB\0\0\2\1\3\0\t-\0\0\0009\0\0\0+\1\2\0=\1\1\0-\0\0\0009\0\0\0)\1,\1=\1\2\0K\0\1\0\0\0\15timeoutlen\ftimeout\6o\0" },
+    loaded = true,
+    path = "/Users/eduardoneville82/.local/share/nvim/site/pack/packer/start/which-key.nvim",
+    url = "https://github.com/folke/which-key.nvim"
   }
 }
 
@@ -282,10 +288,6 @@ if not vim.g.packer_custom_loader_enabled then
   vim.g.packer_custom_loader_enabled = true
 end
 
--- Config for: nvim-tree.lua
-time([[Config for nvim-tree.lua]], true)
-try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
-time([[Config for nvim-tree.lua]], false)
 -- Config for: barbecue.nvim
 time([[Config for barbecue.nvim]], true)
 try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rbarbecue\frequire\0", "config", "barbecue.nvim")
@@ -294,6 +296,14 @@ time([[Config for barbecue.nvim]], false)
 time([[Config for ChatGPT.nvim]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\fchatgpt\frequire\0", "config", "ChatGPT.nvim")
 time([[Config for ChatGPT.nvim]], false)
+-- Config for: nvim-tree.lua
+time([[Config for nvim-tree.lua]], true)
+try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
+time([[Config for nvim-tree.lua]], false)
+-- Config for: which-key.nvim
+time([[Config for which-key.nvim]], true)
+try_loadstring("\27LJ\2\nB\0\0\2\1\3\0\t-\0\0\0009\0\0\0+\1\2\0=\1\1\0-\0\0\0009\0\0\0)\1,\1=\1\2\0K\0\1\0\0\0\15timeoutlen\ftimeout\6o\0", "config", "which-key.nvim")
+time([[Config for which-key.nvim]], false)
 vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Filetype lazy-loads
