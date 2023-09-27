bcb7c0b1c4e0b145068a3245b17774a072173680
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-09-27 16:30:40 +0200

    last config before OS course

--- configs/.config/nvim/lua/base.lua
+++ configs/.config/nvim/lua/base.lua
@@ -28,15 +28,18 @@ vim.opt.clipboard:append {'unnamedplus'}
 --- Tabs ------------------------
 local languages = {
     {4, {'python', 'lua'}},
-    {2, {'javascript','typescript', 'html', 'css'}}
+    {2, {'javascript','typescript', 'html', 'css', 'typescriptreact'}}
 }
 
 vim.cmd('autocmd FileType python setlocal tabstop=4 shiftwidth=4')
 vim.cmd('autocmd FileType lua setlocal tabstop=4 shiftwidth=4')
 vim.cmd('autocmd FileType javascript setlocal tabstop=2 shiftwidth=2')
 vim.cmd('autocmd FileType typescript setlocal tabstop=2 shiftwidth=2')
+vim.cmd('autocmd FileType typescriptreact setlocal tabstop=2 shiftwidth=2')
 vim.cmd('autocmd FileType html setlocal tabstop=2 shiftwidth=2')
 vim.cmd('autocmd FileType css setlocal tabstop=2 shiftwidth=2')
+vim.cmd('autocmd FileType c setlocal tabstop=2 shiftwidth=2')
+vim.cmd('autocmd FileType cpp setlocal tabstop=2 shiftwidth=2')
 
 
 for _, kv in ipairs(languages) do
@@ -44,14 +47,7 @@ for _, kv in ipairs(languages) do
     local languages = kv[2]
 
     for _, language in ipairs(languages) do
-        vim.api.nvim_create_autocmd('FileType', {
-            desc = 'Local tabstop',
-            pattern = language,
-            callback = function (opts)
-                vim.api.nvim_set_option('tabstop', tabstop)
-                vim.api.nvim_set_option('shiftwidth', tabstop)
-            end,
-        })
+        vim.cmd('autocmd FileType language setlocal tabstop=tabstop shiftwidth=tabstop')
     end
 end
 
