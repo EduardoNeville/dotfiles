c12cece554305b9c4e21995df2dd0838c2ed4342
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-10-20 16:22:03 +0200

    fixed some tmux configs 

--- configs/.config/nvim/lua/base.lua
+++ configs/.config/nvim/lua/base.lua
@@ -64,11 +64,13 @@ set_tabstop(languages)
 --- Folds ----------------------
 vim.opt.foldmethod = 'indent'
 vim.opt.number = true
-vim.opt.cursorline = true
 vim.opt.showmode = true
 
 --- Cursor --------------------------
+vim.opt.cursorline = true
 vim.opt.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
+--- vim.cmd [[highlight CursorLine gui=underline guisp=#1C376F]]
+
 --highlight Cursor guifg=white guibg=black
 --highlight iCursor guifg=white guibg=steelblue
 --set guicursor=n-v-c:block-Cursor
