4e36c466829360e7ee8eb52df273382c89abc7ce
EduardoNeville <eduardoneville82@gmail.com>
2023-12-15 06:02:01 +0100

    fixed installer

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -26,7 +26,72 @@ _installHyperland(){
 	sudo apt install libpango1.0-dev -y
 	sudo apt install libgbm-dev -y
 	
+	cd configs
+	mkdir HyprSource
+	cd HyprSource
 
+	## We get Source
+	wget https://github.com/hyprwm/Hyprland/releases/download/v0.24.1/source-v0.24.1.tar.gz
+	tar -xvf source-v0.24.1.tar.gz
+
+	wget https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.31/downloads/wayland-protocols-1.31.tar.xz
+	tar -xvJf wayland-protocols-1.31.tar.xz
+
+	wget https://gitlab.freedesktop.org/wayland/wayland/-/releases/1.22.0/downloads/wayland-1.22.0.tar.xz
+	tar -xvJf wayland-1.22.0.tar.xz
+
+	wget https://gitlab.freedesktop.org/emersion/libdisplay-info/-/releases/0.1.1/downloads/libdisplay-info-0.1.1.tar.xz
+	tar -xvJf libdisplay-info-0.1.1.tar.xz
+
+	cd wayland-1.22.0
+	mkdir build &&
+	cd    build &&
+
+	meson setup ..            \
+	      --prefix=/usr       \
+	      --buildtype=release \
+	      -Ddocumentation=false &&
+	ninja
+	sudo ninja install
+
+	cd ../..
+	cd wayland-protocols-1.31
+
+	mkdir build &&
+	cd    build &&
+
+	meson setup --prefix=/usr --buildtype=release &&
+	ninja
+
+	sudo ninja install
+
+	cd ../..
+	cd libdisplay-info-0.1.1/
+
+	mkdir build &&
+	cd    build &&
+
+	meson setup --prefix=/usr --buildtype=release &&
+	ninja
+
+	sudo ninja install
+
+	cd ../..
+	chmod a+rw hyprland-source
+
+	cd hyprland-source/
+	sed -i 's/\/usr\/local/\/usr/g' config.mk
+	cd ../
+	git clone https://gitlab.freedesktop.org/emersion/libliftoff.git
+	cd libliftoff
+	meson setup build/
+	ninja -C build/
+	cd build
+	sudo ninja install 
+	cd ../../
+	sudo apt install libsystemd-dev
+	cd hyprland-source
+	sudo make install
 }
 
 # ----------------------------------------------------------------------------------------
