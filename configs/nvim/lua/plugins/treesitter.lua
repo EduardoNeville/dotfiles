6d3c853d4ac89c3479f7a0ae023f2e452370c86a
EduardoNeville <eduardoneville82@gmail.com>
2025-12-29 13:01:29 +0100

    fix(nvim) treesitter full config

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,59 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    opt = {},
-    config = function(opt)
+    build = ":TSUpdate",
+    config = function()
         local status, ts = pcall(require, "nvim-treesitter.configs")
         if (not status) then return end
-        ts.build = ":TSUpdate"
         ts.setup {
             highlight = {
                 enable = true,
+                additional_vim_regex_highlighting = false,
                 disable = {},
             },
             indent = {
                 enable = true,
                 disable = {},
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
+                    },
+                },
             },
             ensure_installed = {
                 "markdown",
@@ -31,7 +72,8 @@ return {
                 "javascript",
                 "typescript",
                 "bash",
-                "kotlin"
+                "kotlin",
+                "yaml"
             },
             autotag = {
                 enable = true,
